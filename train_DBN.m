6ac62ea65eebeee2a0a5209c52503ac4e08a5404
Freestar51 <freestar51@unist.ac.kr>
2015-07-02 23:33:20 +0900

    DBN trial
    DBN trial

--- train_DBN.m
+++ train_DBN.m
@@ -1,5 +1,5 @@
 addpath(genpath('./'));
-train_path = './data/train/';
+train_path = '../Dataset/EEG_detection/train/';
 data_list = {};
 event_list = {};
 for sidx=1:12
@@ -14,6 +14,7 @@ end
 rand_idx = randperm(12*8);
 x_dim = 32;
 y_dim = 6;
+disp('dbn setup...');
 for cidx = 1:y_dim
     dbn.sizes = [100 100];
     opts.numepochs =   10;
@@ -23,31 +24,44 @@ for cidx = 1:y_dim
     model{cidx} = dbnsetup(dbn, zeros(1,32), opts);
 end
 
-for batch = 1:size(rand_idx)
-    train_data = read_data_csv(strcat(train_path,data_list{rand_idx(batch)}));
-    parfor midx = 1:y_dim
-        model{midx} = dbntrain(model{midx}, train_data, opts);
+if(exist('./dbn_pretrained.mat'))
+    disp('loading dbn...');
+    load('./dbn_pretrained.mat');
+else
+    disp('dbn pretraining...');
+    for batch = 1:size(rand_idx)
+        train_data = read_data_csv(strcat(train_path,data_list{rand_idx(batch)}));
+        train_data = train_data(1:(end-mod(size(train_data,1),100)),:);
+        parfor midx = 1:y_dim
+            model{midx} = dbntrain(model{midx}, train_data, opts);
+        end
     end
+    disp('dbn model saving...');
+    save('./dbn_pretrained.mat','model');
 end
-
-save('./dbn_pretrained.mat','model');
-
-
-parfor midx = 1:y_dim
-    model{midx} = dbnunfoldtonn(model{midx}, 2);
-    model{midx}.activation_function = 'sigm';
-end
-
-for batch = 1:size(rand_idx)
-    train_data = read_data_csv(strcat(train_path,data_list{rand_idx(batch)}));
-    train_label = read_label_csv(strcat(train_path,event_list{rand_idx(batch)}));
+if(exist('./nn_trained.mat'))
+    disp('loading nn...');
+    load('./nn_trained.mat');
+else
+    disp('nn training...');
     parfor midx = 1:y_dim
-        model{midx} = nntrain(model{midx}, train_data, train_label(:,midx), opts);
+        model{midx} = dbnunfoldtonn(model{midx}, 2);
+        model{midx}.activation_function = 'sigm';
     end
+
+    for batch = 1:size(rand_idx)
+        train_data = read_data_csv(strcat(train_path,data_list{rand_idx(batch)}));
+        train_label = read_label_csv(strcat(train_path,event_list{rand_idx(batch)}));
+        train_data = train_data(1:(end-mod(size(train_data,1),100)),:);
+        train_label = train_label(1:(end-mod(size(train_label,1),100)),:);
+        parfor midx = 1:y_dim
+            model{midx} = nntrain(model{midx}, train_data, [~train_label(:,midx),train_label(:,midx)], opts);
+        end
+    end
+
+    disp('nn model saving...');
+    save('./nn_trained.mat','model');
 end
-
-save('./nn_trained.mat','model');
-
 % parfor midx = 1:y_dim
 %     pred(:,midx) = nnpredict(model{midx}, test_x);
 % end
